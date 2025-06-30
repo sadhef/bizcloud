fe751cf2f7744d2df1fd127d799238c823a4cd4b
sadhef <muhammedsadhef@gmail.com>
2025-07-01 00:05:08 +0530

    Resolved merge conflict in biztras-frontend

--- biztras-backend/server.js
+++ biztras-backend/server.js
@@ -16,7 +16,7 @@ const corsOptions = {
     
     const allowedOrigins = [
       'http://localhost:3000',
-      'http://127.0.0.1:3000',
+      'https://bizcloud-beta.vercel.app',
       process.env.FRONTEND_URL
     ].filter(Boolean);
     
